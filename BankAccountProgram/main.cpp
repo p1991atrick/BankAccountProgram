369172ad9dcf4a4b21ce4a50e65a815b422adaaf
Patrick A Kelly <p1991atrick@gmail.com>
2016-01-31 13:51:26 -0800

    Initial Commit

---
+++ BankAccountProgram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BankAccountProgram
+//
+//  Created by Gordon Freeman on 1/31/16.
+//  Copyright © 2016 Patrick Kelly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
