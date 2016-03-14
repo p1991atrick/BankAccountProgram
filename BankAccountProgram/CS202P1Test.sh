8cd7b521f26d61ee98875955a9c59c3b246ce97d
Patrick A Kelly <p1991atrick@gmail.com>
2016-03-14 14:36:47 -0700

    test 13

--- BankAccountProgram/CS202P1Test.sh
+++ BankAccountProgram/CS202P1Test.sh
@@ -7,24 +7,6 @@
 #
 
 #vars
-#echo "Enter file name:"
-#read FILE
-#echo "Enter First name to use:"
-#read FNAME
-#echo "Enter Last name to use:"
-#read LNAME
-#echo "Enter Middle Initial to use:"
-#read MIDDLE
-#echo "Enter SSN to use:"
-#read SSN
-#echo "Enter Phone Number Area Code to use:"
-#read AREA
-#echo "Enter Phone Number to use:"
-#read PHONE
-#echo "Enter Balance to use:"
-#read AMMT
-#echo "Enter New Password to use:"
-#read NEWPASS
 FILE=Test
 FNAME=JONNY
 LNAME=Smithers
@@ -35,47 +17,89 @@ PHONE=5846958
 AMMT=584.32
 NEWPASS=POIU00
 
-ACC_1=AA001
-PAS_1=POIU01
-ACC_2=C123A
-PAS_2=Z52C42
-ACC_3=AA004
-PAS_3=POIU04
+MAN="$1" || "0"
 
-VERBOSE="$1" || "0"
+if [ "$MAN" == "1" ]; then
+	echo "Enter file name to use:"
+	read FILE
+	echo "Enter First name to use:"
+	read FNAME
+	echo "Enter Last name to use:"
+	read LNAME
+	echo "Enter Middle Initial to use:"
+	read MIDDLE
+	echo "Enter SSN to use:"
+	read SSN
+	echo "Enter Phone Number Area Code to use:"
+	read AREA
+	echo "Enter Phone Number to use:"
+	read PHONE
+	echo "Enter Balance to use:"
+	read AMMT
+	echo "Enter New Password to use:"
+	read NEWPASS
+else
+	ACC_1=AA001
+	PAS_1=POIU01
+	ACC_2=C123A
+	PAS_2=Z52C42
+	ACC_3=AA004
+	PAS_3=POIU04
+fi
+
+VERBOSE="$2" || "0"
 
 #commands
 
-# Display account
+# Verbose check
 if [ "$VERBOSE" == "1" ]; then
+	# Display account
 	./bankacct /V /D${FILE} /N${ACC_1} /P${PAS_1} /I
+	# Change First Name
+	./bankacct /V /D${FILE} /N${ACC_1} /P${PAS_1} /F${FNAME}
+	# Change Last Name
+	./bankacct /V /D${FILE} /N${ACC_2} /P${PAS_2} /L${LNAME}
+	# Change Middle Initial
+	./bankacct /V /D${FILE} /N${ACC_3} /P${PAS_3} /M${MIDDLE}
+	# Change SSN
+	./bankacct /V /D${FILE} /N${ACC_1} /P${PAS_1} /S${SSN}
+	# Change Phone Number Area Code
+	./bankacct /V /D${FILE} /N${ACC_1} /P${PAS_1} /A${AREA}
+	# Change Phone Number
+	./bankacct /V /D${FILE} /N${ACC_1} /P${PAS_1} /H${PHONE}
+	# Transfer money
+	./bankacct /V /D${FILE} /N${ACC_1} /P${PAS_1} /N${ACC_2} /P${PAS_2} /T${AMMT}
+	# Change password
+	./bankacct /V /D${FILE} /N${ACC_1} /P${PAS_1} /W${NEWPASS}
+	# Print Report file
+	./bankacct /V /R${RFILE} /D${FILE}
 else
+	# Display account
 	./bankacct /D${FILE} /N${ACC_1} /P${PAS_1} /I
+	# Change First Name
+	./bankacct /D${FILE} /N${ACC_1} /P${PAS_1} /F${FNAME}
+	# Change Last Name
+	./bankacct /D${FILE} /N${ACC_2} /P${PAS_2} /L${LNAME}
+	# Change Middle Initial
+	./bankacct /D${FILE} /N${ACC_3} /P${PAS_3} /M${MIDDLE}
+	# Change SSN
+	./bankacct /D${FILE} /N${ACC_1} /P${PAS_1} /S${SSN}
+	# Change Phone Number Area Code
+	./bankacct /D${FILE} /N${ACC_1} /P${PAS_1} /A${AREA}
+	# Change Phone Number
+	./bankacct /D${FILE} /N${ACC_1} /P${PAS_1} /H${PHONE}
+	# Transfer money
+	./bankacct /D${FILE} /N${ACC_1} /P${PAS_1} /N${ACC_2} /P${PAS_2} /T${AMMT}
+	# Change password
+	./bankacct /D${FILE} /N${ACC_1} /P${PAS_1} /W${NEWPASS}
+	# Print Report file
+	./bankacct /R${RFILE} /D${FILE}
 fi
 
-# Change First name
-./bankacct /V /D${FILE} /N${ACC_1} /P${PAS_1} /F${FNAME}
 
-# Change Last name
-./bankacct /V /D${FILE} /N${ACC_2} /P${PAS_2} /L${LNAME}
 
-# Change Middle initial
-./bankacct /V /D${FILE} /N${ACC_3} /P${PAS_3} /M${MIDDLE}
 
-# Change ssn
-./bankacct /V /D${FILE} /N${ACC_1} /P${PAS_1} /S${SSN}
 
-# Change phonenumber area code
-./bankacct /V /D${FILE} /N${ACC_1} /P${PAS_1} /A${AREA}
 
-# Change phonenumber area code
-./bankacct /D${FILE} /N${ACC_1} /P${PAS_1} /H${PHONE}
 
-# Transfer money
-./bankacct /D${FILE} /N${ACC_1} /P${PAS_1} /N${ACC_2} /P${PAS_2} /T${AMMT}
 
-# Change password
-./bankacct /D${FILE} /N${ACC_1} /P${PAS_1} /W${NEWPASS}
-
-# Print Report file
-./bankacct /R${RFILE} /D${FILE}
