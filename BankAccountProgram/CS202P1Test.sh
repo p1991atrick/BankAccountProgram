6d0eba9083f2d968e09dbc9cf127e411563ddd51
Patrick A Kelly <p1991atrick@gmail.com>
2016-03-14 12:27:09 -0700

    updated script file

--- BankAccountProgram/CS202P1Test.sh
+++ BankAccountProgram/CS202P1Test.sh
@@ -7,52 +7,61 @@
 #
 
 #vars
-FILE=Test
-RFILE=
-FNAME=Johnny
-LNAME=Smith
-MIDDLE=X
-SSN=453658741
-AREA=557
-PHONE=6548521
-AMMT=653.26
-NEWPASS=UIOP00
+echo "Enter file name:"
+read FILE
+echo "Enter First name to use:"
+read FNAME
+echo "Enter Last name to use:"
+read LNAME
+echo "Enter Middle Initial to use:"
+read MIDDLE
+echo "Enter SSN to use:"
+read SSN
+echo "Enter Phone Number Area Code to use:"
+read AREA
+echo "Enter Phone Number to use:"
+read PHONE
+echo "Enter Balance to use:"
+read AMMT
+echo "Enter New Password to use:"
+read NEWPASS
 
-ACC1=AA001
-PAS1=POIU01
-ACC2=C123A
-PAS2=Z52C42
-ACC3=AA004
-PAS3=POIU04
+
+ACC_1=AA001
+PAS_1=POIU01
+ACC_2=C123A
+PAS_2=Z52C42
+ACC_3=AA004
+PAS_3=POIU04
 
 #commands
 
 # Display account
-./bankacct /D$(FILE) /N$(ACC1) /P$(PAS1) /I
+./bankacct /D$FILE /N$ACC_1 /P$PAS_1 /I
 
 # Change First name
-./bankacct /D$(FILE) /N$(ACC1) /P$(PAS1) /F$(FNAME)
+./bankacct /D$FILE /N$ACC_1 /P$PAS_1 /F$FNAME
 
 # Change Last name
-./bankacct /D$(FILE) /N$(ACC2) /P$(PAS2) /L$(LNAME)
+./bankacct /D$FILE /N$ACC_2 /P$PAS_2 /L$LNAME
 
 # Change Middle initial
-./bankacct /D$(FILE) /N$(ACC3) /P$(PAS3 ) /M$(MIDDLE)
+./bankacct /D$FILE /N$ACC_3 /P$PAS_3  /M$MIDDLE
 
 # Change ssn
-./bankacct /D$(FILE) /N$(ACC1) /P$(PAS1) /S$(SSN)
+./bankacct /D$FILE /N$ACC_1 /P$PAS_1 /S$SSN
 
 # Change phonenumber area code
-./bankacct /D$(FILE) /N$(ACC1) /P$(PAS1) /A$(AREA)
+./bankacct /D$FILE /N$ACC_1 /P$PAS_1 /A$AREA
 
 # Change phonenumber area code
-./bankacct /D$(FILE) /N$(ACC1) /P$(PAS1) /H$(PHONE)
+./bankacct /D$FILE /N$ACC_1 /P$PAS_1 /H$PHONE
 
 # Transfer money
-./bankacct /D$(FILE) /N$(ACC1) /P$(PAS1) /N$(ACC2) /P$(PAS2) /T$(AMMT)
+./bankacct /D$FILE /N$ACC_1 /P$PAS_1 /N$ACC_2 /P$PAS_2 /T$AMMT
 
 # Change password
-./bankacct /D$(FILE) /N$(ACC1) /P$(PAS1) /W$(NEWPASS)
+./bankacct /D$FILE /N$ACC_1 /P$PAS_1 /W$NEWPASS
 
 # Print Report file
-./bankacct /R$(RFILE) /D$(FILE)
+./bankacct /R$RFILE /D$FILE
